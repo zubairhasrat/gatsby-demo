a705e9950ad3b95b81b0d3daf3bc713a251ca658
ZubairAhmed <zubairahmed@A006-00258.arbisoft.com>
2020-01-16 18:20:13 +0500

    Update config-gatsby.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,11 +16,10 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
-        contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
-          'article',
-          'user'
-        ],
+        apiURL: process.env.DEPLOY_URL
+          ? "https://strapi-gatsby-starter.herokuapp.com"
+          : "http://localhost:1337",
+        contentTypes: [`article`, `user`],
         queryLimit: 1000,
       },
     },
